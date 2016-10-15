e6dc12f958ede69443be6023020076452eaf21de
ArsenArsen <ArsenArsen@users.noreply.github.com>
2016-10-15 23:10:25 +0200

    Add playlist support

--- src/main/java/com/bwfcwalshy/flarebot/VideoThread.java
+++ src/main/java/com/bwfcwalshy/flarebot/VideoThread.java
@@ -1,6 +1,7 @@
 package com.bwfcwalshy.flarebot;
 
 import com.bwfcwalshy.flarebot.music.MusicManager;
+import com.google.gson.JsonParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
+import java.util.List;
 
 public class VideoThread extends Thread {
 
@@ -61,42 +63,44 @@ public class VideoThread extends Thread {
             String videoName;
             String link;
             String videoId;
-            if (isUrl) {
-                if (isShortened) {
-                    searchTerm = YOUTUBE_URL + searchTerm.replaceFirst("http(s)?://youtu\\.be", "");
-                }
-                message = MessageUtils.sendMessage(channel, "Getting video from URL.");
-                Document doc = Jsoup.connect(searchTerm).get();
-                videoId = searchTerm.replaceFirst("http(s)?://(www\\.)?youtube\\.com/watch\\?v=", "");
-                // Playlist
-                if (videoId.contains("&list")) videoId.substring(0, videoId.indexOf("&list") + 5);
-                videoName = MessageUtils.escapeFile(doc.title().replace(" - YouTube", ""));
-                link = searchTerm;
-            } else {
-                message = MessageUtils.sendMessage(channel, "Searching YouTube for '" + searchTerm + "'");
-                Document doc = Jsoup.connect(SEARCH_URL + URLEncoder.encode(searchTerm, "UTF-8")).get();
-
-                int i = 0;
-                Element videoElement = doc.getElementsByClass("yt-lockup-title").get(i);
-                boolean hasAd = true;
-                while (hasAd) {
-                    for (Element e : videoElement.children()) {
-                        if (e.toString().contains("href=\"https://googleads")) {
-                            videoElement = doc.getElementsByClass("yt-lockup-title").get(++i);
-                            break;
-                        }
+            if (!searchTerm.matches("https?://(www\\.)?youtube.com/playlist\\?list=([0-9A-z+-]*)")) {
+                if (isUrl) {
+                    message = MessageUtils.sendMessage(channel, "Getting video from URL.");
+                    if (isShortened) {
+                        searchTerm = YOUTUBE_URL + searchTerm.replaceFirst("http(s)?://youtu\\.be", "");
                     }
-                    hasAd = false;
-                }
-                link = videoElement.select("a").first().attr("href");
-                Document doc2 = Jsoup.connect((link.startsWith("http") ? "" : YOUTUBE_URL) + link).get();
-                videoName = MessageUtils.escapeFile(doc2.title().substring(0, doc2.title().length() - 10));
-                // I check the index of 2 chars so I need to add 2
-                videoId = link.substring(link.indexOf("v=") + 2);
 
-                link = YOUTUBE_URL + link;
-            }
-            File video = new File("cached" + File.separator + videoId + EXTENSION);
+                    Document doc = Jsoup.connect(searchTerm).get();
+                    videoId = searchTerm.replaceFirst("http(s)?://(www\\.)?youtube\\.com/watch\\?v=", "");
+                    // Playlist
+                    if (videoId.contains("&list")) videoId.substring(0, videoId.indexOf("&list") + 5);
+                    videoName = MessageUtils.escapeFile(doc.title().replace(" - YouTube", ""));
+                    link = searchTerm;
+                } else {
+                    message = MessageUtils.sendMessage(channel, "Searching YouTube for '" + searchTerm + "'");
+                    Document doc = Jsoup.connect(SEARCH_URL + URLEncoder.encode(searchTerm, "UTF-8")).get();
+
+                    int i = 0;
+                    Element videoElement = doc.getElementsByClass("yt-lockup-title").get(i);
+                    boolean hasAd = true;
+                    while (hasAd) {
+                        for (Element e : videoElement.children()) {
+                            if (e.toString().contains("href=\"https://googleads")) {
+                                videoElement = doc.getElementsByClass("yt-lockup-title").get(++i);
+                                break;
+                            }
+                        }
+                        hasAd = false;
+                    }
+                    link = videoElement.select("a").first().attr("href");
+                    Document doc2 = Jsoup.connect((link.startsWith("http") ? "" : YOUTUBE_URL) + link).get();
+                    videoName = MessageUtils.escapeFile(doc2.title().substring(0, doc2.title().length() - 10));
+                    // I check the index of 2 chars so I need to add 2
+                    videoId = link.substring(link.indexOf("v=") + 2);
+
+                    link = YOUTUBE_URL + link;
+                }
+                File video = new File("cached" + File.separator + videoId + EXTENSION);
 //            if (video.exists()) {
 //                manager.addSong(channel.getGuild().getID(), videoFile + EXTENSION);
 //                RequestBuffer.request(() -> {
@@ -106,60 +110,117 @@ public class VideoThread extends Thread {
 //                        FlareBot.LOGGER.error("Could not edit own message!", e);
 //                    }
 //                });
-            if (!video.exists()) {
-                RequestBuffer.request(() -> {
-                    try {
-                        message.edit("Downloading video!");
-                    } catch (MissingPermissionsException | DiscordException e) {
-                        FlareBot.LOGGER.error("Could not edit own message!", e);
-                    }
-                });
-                ProcessBuilder builder = new ProcessBuilder("youtube-dl", "-o",
-                        "cached" + File.separator + "%(id)s.%(ext)s",
-                        "--extract-audio", "--audio-format"
-                        , "mp3", link);
-                FlareBot.LOGGER.debug("Downloading");
-                builder.redirectErrorStream(true);
-                Process process = builder.start();
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                    while (process.isAlive()) {
-                        String line;
-                        if ((line = reader.readLine()) != null) {
-                            FlareBot.LOGGER.info("[YT-DL] " + line);
-                        }
-                    }
-                }
-                process.waitFor();
-                if (process.exitValue() != 0) {
+                if (!video.exists()) {
                     RequestBuffer.request(() -> {
                         try {
-                            message.edit("Could not download **" + videoName + "**!");
+                            message.edit("Downloading video!");
                         } catch (MissingPermissionsException | DiscordException e) {
                             FlareBot.LOGGER.error("Could not edit own message!", e);
                         }
                     });
-                    return;
+                    ProcessBuilder builder = new ProcessBuilder("youtube-dl", "-o",
+                            "cached" + File.separator + "%(id)s.%(ext)s",
+                            "--extract-audio", "--audio-format"
+                            , "mp3", link);
+                    FlareBot.LOGGER.debug("Downloading");
+                    builder.redirectErrorStream(true);
+                    Process process = builder.start();
+                    processInput(process);
+                    process.waitFor();
+                    if (process.exitValue() != 0) {
+                        RequestBuffer.request(() -> {
+                            try {
+                                message.edit("Could not download **" + videoName + "**!");
+                            } catch (MissingPermissionsException | DiscordException e) {
+                                FlareBot.LOGGER.error("Could not edit own message!", e);
+                            }
+                        });
+                        return;
+                    }
                 }
-            }
-            if (manager.addSong(channel.getGuild().getID(), video.getName(), videoName)) {
-                RequestBuffer.request(() -> {
+                if (manager.addSong(channel.getGuild().getID(), video.getName(), videoName)) {
+                    RequestBuffer.request(() -> {
+                        try {
+                            message.edit(user.mention() + " added: **" + videoName + "** to the playlist!");
+                        } catch (MissingPermissionsException | DiscordException e) {
+                            FlareBot.LOGGER.error("Could not edit own message!", e);
+                        }
+                    });
+                } else RequestBuffer.request(() -> {
                     try {
-                        message.edit(user.mention() + " added: **" + videoName + "** to the playlist!");
+                        message.edit("Failed to add **" + videoName + "**!");
                     } catch (MissingPermissionsException | DiscordException e) {
                         FlareBot.LOGGER.error("Could not edit own message!", e);
                     }
                 });
-            } else RequestBuffer.request(() -> {
+            } else {
+                message = MessageUtils.sendMessage(channel, "Getting playlist from URL.");
+                ProcessBuilder builder = new ProcessBuilder("youtube-dl", "-i", "-4", "--dump-single-json", searchTerm);
+                searchTerm = searchTerm.replaceFirst("https?://(www\\.)?youtube.com/playlist\\?list=", "");
+                Process process = builder.start();
+                Playlist playlist;
                 try {
-                    message.edit("Failed to add **" + videoName + "**!");
-                } catch (MissingPermissionsException | DiscordException e) {
-                    FlareBot.LOGGER.error("Could not edit own message!", e);
+                    playlist = FlareBot.GSON.fromJson(new InputStreamReader(process.getInputStream()), Playlist.class);
+                } catch (JsonParseException e) {
+                    RequestBuffer.request(() -> {
+                        try {
+                            message.edit("Could not parse the playlist!");
+                        } catch (MissingPermissionsException | DiscordException e1) {
+                            FlareBot.LOGGER.error("Edit own message!", e1);
+                        }
+                    });
+                    FlareBot.LOGGER.error("Could not parse playlist!", e);
+                    return;
                 }
-            });
+                RequestBuffer.request(() -> {
+                    try {
+                        message.edit("Downloading **" + playlist.title + "**");
+                    } catch (MissingPermissionsException | DiscordException e1) {
+                        FlareBot.LOGGER.error("Edit own message!", e1);
+                    }
+                });
+                for (Playlist.PlaylistEntry e : playlist.entries) {
+                    if(!new File("cached" + File.separator + e.id + EXTENSION).exists()){
+                        ProcessBuilder entryDownload = new ProcessBuilder("youtube-dl", "-o",
+                                "cached" + File.separator + "%(id)s.%(ext)s",
+                                "--extract-audio", "--audio-format"
+                                , "mp3", YOUTUBE_URL + "/watch?v="+e.id);
+                        FlareBot.LOGGER.debug("Downloading");
+                        builder.redirectErrorStream(true);
+                        Process downloadProcess = builder.start();
+                        processInput(downloadProcess);
+                        process.waitFor();
+                    }
+                    FlareBot.getInstance().getMusicManager().addSong(message.getChannel().getGuild().getID(), e.id, e.title);
+                }
+            }
         } catch (IOException | InterruptedException e) {
             FlareBot.LOGGER.error(e.getMessage(), e);
         }
+
         long b = System.currentTimeMillis();
         FlareBot.LOGGER.debug("Process took " + (b - a) + " milliseconds");
+    }
+
+    private void processInput(Process downloadProcess) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(downloadProcess.getInputStream()))) {
+            while (downloadProcess.isAlive()) {
+                String line;
+                if ((line = reader.readLine()) != null) {
+                    FlareBot.LOGGER.info("[YT-DL] " + line);
+                }
+            }
+        }
+    }
+
+    private class Playlist {
+        public List<PlaylistEntry> entries;
+
+        public class PlaylistEntry {
+            public String id;
+            public String title;
+        }
+
+        public String title;
     }
 }
