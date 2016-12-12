57dd6493b8a92dde72f2dcb37f380b57742e7c8d
ArsenArsen <ArsenArsen@users.noreply.github.com>
2016-12-12 20:18:08 +0100

    Fix search?

--- src/main/java/com/bwfcwalshy/flarebot/music/extractors/YouTubeSearchExtractor.java
+++ src/main/java/com/bwfcwalshy/flarebot/music/extractors/YouTubeSearchExtractor.java
@@ -2,28 +2,31 @@ package com.bwfcwalshy.flarebot.music.extractors;
 
 import com.arsenarsen.lavaplayerbridge.player.Player;
 import com.bwfcwalshy.flarebot.MessageUtils;
-import com.sedmelluq.discord.lavaplayer.track.AudioItem;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
+
+import java.net.URLEncoder;
+import java.util.List;
 
 public class YouTubeSearchExtractor extends YouTubeExtractor {
     public static final String SEARCH_URL = "https://www.youtube.com/results?search_query=";
 
     @Override
     public void process(String input, Player player, IMessage message, IUser user) throws Exception {
+        Document doc = Jsoup.connect(SEARCH_URL + URLEncoder.encode(input, "UTF-8")).get();
+        int i = 0;
         String link = null;
-        AudioItem result = player.resolve("ytsearch: " + input);
-        if (result instanceof AudioPlaylist) {
-            AudioPlaylist res = (AudioPlaylist) result;
-            if(!res.getTracks().isEmpty()){
-                link = WATCH_URL + res.getTracks().get(0).getIdentifier();
-            }
+        List<Element> videoElements = doc.getElementsByClass("yt-lockup-title");
+        for (Element result : videoElements) {
+            link = result.select("a").first().attr("abs:href");
+            if(super.valid(link))
+                break;
         }
         if (link == null) {
-            MessageUtils.editMessage("", MessageUtils.getEmbed(user)
-                    .withDesc(String.format("Found no results for **%s**!", input))
-                    .build(), message);
+            MessageUtils.editMessage(MessageUtils.getEmbed(user).withDesc("Could not find any results for **" + input + "**!").build(), message);
             return;
         }
         super.process(link, player, message, user);
