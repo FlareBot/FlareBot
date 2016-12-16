e714e1457831eb59d9fdf7686ede397400efe5da
ArsenArsen <ArsenArsen@users.noreply.github.com>
2016-12-16 20:44:31 +0100

    Add search playlist support

--- src/main/java/com/bwfcwalshy/flarebot/music/extractors/YouTubeSearchExtractor.java
+++ src/main/java/com/bwfcwalshy/flarebot/music/extractors/YouTubeSearchExtractor.java
@@ -3,6 +3,7 @@ package com.bwfcwalshy.flarebot.music.extractors;
 import com.arsenarsen.lavaplayerbridge.player.Player;
 import com.bwfcwalshy.flarebot.FlareBot;
 import com.mashape.unirest.http.Unirest;
+import org.json.JSONObject;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
@@ -13,9 +14,16 @@ public class YouTubeSearchExtractor extends YouTubeExtractor {
 
     @Override
     public void process(String input, Player player, IMessage message, IUser user) throws Exception {
-        String link = Unirest.get(String.format("https://www.googleapis.com/youtube/v3/search?q=%s&part=snippet&key=%s&maxResults=1",
+        JSONObject result = Unirest.get(String.format("https://www.googleapis.com/youtube/v3/search?q=%s&part=snippet&key=%s&maxResults=1",
                 URLEncoder.encode(input, "UTF-8"), FlareBot.getYoutubeKey())).asJson().getBody()
-                .getObject().getJSONArray("items").getJSONObject(0).getJSONObject("id").getString("videoId");
+                .getObject().getJSONArray("items").getJSONObject(0);
+        JSONObject id = result.getJSONObject("id");
+        String link;
+        if(id.getString("kind").equals("youtube#playlist")){
+            link = PLAYLIST_URL + id.getString("id");
+        } else {
+            link = WATCH_URL + id.getString("id");
+        }
         super.process(link, player, message, user);
     }
 }
