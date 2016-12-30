d0df9165ba4c653c2f2c328e453ff4ca784098bd
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2016-12-30 10:10:32 +0000

    * Added Joda-Time * Created scheduled updates and queued updates

--- src/main/java/com/bwfcwalshy/flarebot/commands/secret/UpdateCommand.java
+++ src/main/java/com/bwfcwalshy/flarebot/commands/secret/UpdateCommand.java
@@ -4,6 +4,11 @@ import com.bwfcwalshy.flarebot.FlareBot;
 import com.bwfcwalshy.flarebot.MessageUtils;
 import com.bwfcwalshy.flarebot.commands.Command;
 import com.bwfcwalshy.flarebot.commands.CommandType;
+import com.bwfcwalshy.flarebot.scheduler.FlarebotTask;
+import java.util.concurrent.TimeUnit;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
@@ -19,34 +24,75 @@ public class UpdateCommand implements Command {
 
     public static volatile AtomicBoolean updating = new AtomicBoolean(false);
 
+    private FlareBot flareBot = FlareBot.getInstance();
+
     @Override
     public void onCommand(IUser sender, IChannel channel, IMessage message, String[] args) {
         if(getPermissions(channel).isCreator(sender)){
-            try {
-                URL url = new URL("https://raw.githubusercontent.com/bwfcwalshyPluginDev/FlareBot/master/pom.xml");
-                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-                String line;
-                while (true) {
-                    line = br.readLine();
-                    if(line != null && (line.contains("<version>") && line.contains("</version>"))){
-                        String latestVersion = line.replace("<version>", "").replace("</version>", "").replaceAll(" ", "").replaceAll("\t", "");
-                        String currentVersion = FlareBot.getInstance().getVersion();
-                        if((args.length == 1 && args[0].equalsIgnoreCase("force")) || isHigher(latestVersion, currentVersion)){
-                            FlareBot.getInstance().getClient().changeStatus(Status.game("Updating.."));
-                            MessageUtils.sendMessage(channel, "Updating to version `" + latestVersion + "` from `" + currentVersion + "`");
-                            updating.set(true);
-                            FlareBot.getInstance().quit(true);
-                        }else{
-                            MessageUtils.sendMessage(channel, "I am currently up to date! Current version: `" + currentVersion + "`");
-                        }
-                        break;
+            if(args.length == 0){
+                update(false, channel);
+            }else if(args.length == 1){
+                if(args[0].equalsIgnoreCase("force")){
+                    update(true, channel);
+                }else if(args[0].equalsIgnoreCase("no-active-channels")){
+                    if(flareBot.getClient().getConnectedVoiceChannels().size() == 0) {
+                        update(true, channel);
+                    }else{
+                        new FlarebotTask("Queued-Update") {
+                            @Override
+                            public void run() {
+                                if(flareBot.getClient().getConnectedVoiceChannels().size() == 0) {
+                                    update(true, channel);
+                                }
+                            }
+                        }.repeat(TimeUnit.MINUTES.toMillis(1), TimeUnit.MINUTES.toMillis(1));
+                    }
+                }else{
+                    try {
+                        PeriodFormatter formatter = new PeriodFormatterBuilder().appendDays().appendSuffix("d").appendHours().appendSuffix("h").appendSeconds().appendSuffix("s")
+                                .toFormatter();
+                        Period p = formatter.parsePeriod(args[0]);
+
+                        new FlarebotTask("Scheduled-Update") {
+                            @Override
+                            public void run() {
+                                update(true, channel);
+                            }
+                        }.delay(p.toStandardSeconds().getSeconds()*1000);
+                    }catch(IllegalArgumentException e){
+                        MessageUtils.sendMessage(channel, "That is an invalid time option!");
                     }
                 }
-            }catch(IOException e){
-                FlareBot.LOGGER.error("Could not update!", e);
             }
         }
     }
+
+    private void update(boolean force, IChannel channel){
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/bwfcwalshyPluginDev/FlareBot/master/pom.xml");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while (true) {
+                line = br.readLine();
+                if(line != null && (line.contains("<version>") && line.contains("</version>"))){
+                    String latestVersion = line.replace("<version>", "").replace("</version>", "").replaceAll(" ", "").replaceAll("\t", "");
+                    String currentVersion = FlareBot.getInstance().getVersion();
+                    if(force || isHigher(latestVersion, currentVersion)){
+                        FlareBot.getInstance().getClient().changeStatus(Status.game("Updating.."));
+                        MessageUtils.sendMessage(channel, "Updating to version `" + latestVersion + "` from `" + currentVersion + "`");
+                        updating.set(true);
+                        FlareBot.getInstance().quit(true);
+                    }else{
+                        MessageUtils.sendMessage(channel, "I am currently up to date! Current version: `" + currentVersion + "`");
+                    }
+                    break;
+                }
+            }
+        }catch(IOException e){
+            FlareBot.LOGGER.error("Could not update!", e);
+        }
+    }
+
 
     /**
      * Check if a string is higher than another.
