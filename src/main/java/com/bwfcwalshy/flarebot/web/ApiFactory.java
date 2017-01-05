56e64ff21372a0ac6bdb0e8237894351aa97ece3
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-01-05 15:16:13 +0100

    Implement setters

--- src/main/java/com/bwfcwalshy/flarebot/web/ApiFactory.java
+++ src/main/java/com/bwfcwalshy/flarebot/web/ApiFactory.java
@@ -20,5 +20,20 @@ public class ApiFactory {
                 return "<pre>" + ExceptionUtils.getStackTrace(e) + "<pre>";
             }
         });
+        Spark.post("/setters/:provider", (request, response) -> {
+            try {
+                DataSetters setters;
+                try {
+                    setters = DataSetters.valueOf(request.params("provider").toUpperCase());
+                } catch (Exception e) {
+                    response.status(404);
+                    return "Unknown setter: " + request.params("provider");
+                }
+                return setters.process(request, response);
+            } catch (Throwable e) {
+                response.status(500);
+                return "<pre>" + ExceptionUtils.getStackTrace(e) + "<pre>";
+            }
+        });
     }
 }
--- src/main/java/com/bwfcwalshy/flarebot/web/DataSetters.java
+++ src/main/java/com/bwfcwalshy/flarebot/web/DataSetters.java
@@ -1,22 +1,21 @@
 package com.bwfcwalshy.flarebot.web;
 
 import com.bwfcwalshy.flarebot.FlareBot;
-import com.bwfcwalshy.flarebot.web.objects.Songs;
 import spark.Request;
 import spark.Response;
 
 import java.util.function.BiFunction;
 
-public enum DataProviders {
-    SONGS((req, res) -> Songs.get());
+public enum DataSetters {
+    ;
 
     private BiFunction<Request, Response, Object> consumer;
 
-    DataProviders(BiFunction<Request, Response, Object> o) {
+    DataSetters(BiFunction<Request, Response, Object> o) {
         consumer = o;
     }
 
-    public String provide(Request request, Response response) {
+    public String process(Request request, Response response) {
         return FlareBot.GSON.toJson(consumer.apply(request, response));
     }
 }
