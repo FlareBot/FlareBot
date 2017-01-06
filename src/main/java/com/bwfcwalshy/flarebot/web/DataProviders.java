f3d5edf79ab2e9e1c1018305ea1b9e3d9f89c252
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-01-06 09:52:26 +0100

    Add permissions to DataProviders.java and DataSetters.java

--- src/main/java/com/bwfcwalshy/flarebot/web/DataProviders.java
+++ src/main/java/com/bwfcwalshy/flarebot/web/DataProviders.java
@@ -8,15 +8,28 @@ import spark.Response;
 import java.util.function.BiFunction;
 
 public enum DataProviders {
-    SONGS((req, res) -> Songs.get());
+    SONGS((req, res) -> Songs.get()),
+    GETPERMISSIONS((request, response) -> FlareBot.getInstance()
+            .getPermissions(FlareBot.getInstance()
+                    .getClient().getChannelByID(request.queryParams("guildid"))),
+            new Require("guildid", gid -> FlareBot.getInstance().getClient().getGuildByID(gid) != null));
 
     private BiFunction<Request, Response, Object> consumer;
+    private Require[] requires;
 
-    DataProviders(BiFunction<Request, Response, Object> o) {
+    DataProviders(BiFunction<Request, Response, Object> o, Require... requires) {
         consumer = o;
+        this.requires = requires;
     }
 
-    public String provide(Request request, Response response) {
+    public String process(Request request, Response response) {
+        for (Require require : requires) {
+            if (!require.verify(request)) {
+                response.status(400);
+                return String.format("Require for <code>%s</code> failed!", require.getName());
+            }
+
+        }
         return FlareBot.GSON.toJson(consumer.apply(request, response));
     }
 }
