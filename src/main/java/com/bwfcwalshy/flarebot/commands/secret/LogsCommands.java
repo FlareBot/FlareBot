29675b3dc4a7f876ab396b05c3b9c978d4d66172
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-02-07 21:14:52 +0100

    Fix LogsCommands.java

--- src/main/java/com/bwfcwalshy/flarebot/commands/secret/LogsCommands.java
+++ src/main/java/com/bwfcwalshy/flarebot/commands/secret/LogsCommands.java
@@ -17,7 +17,7 @@ public class LogsCommands implements Command {
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
         if (getPermissions(channel).isCreator(sender)) {
             try {
-                channel.sendFile(new File("latest.log"), new MessageBuilder().build()).queue();
+                channel.sendFile(new File("latest.log"), new MessageBuilder().append('\u200B').build()).queue();
             } catch (IOException e) {
                 FlareBot.LOGGER.error("Could not send logs", e);
             }
