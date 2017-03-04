3336118697b431582e3c4dc2980da8c1f78cf5cd
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-03-04 14:02:02 +0100

    Implement severity levels and start implementing strike counting

--- src/main/java/com/bwfcwalshy/flarebot/mod/SeverityLevel.java
+++ src/main/java/com/bwfcwalshy/flarebot/mod/SeverityLevel.java
@@ -1,4 +1,24 @@
 package com.bwfcwalshy.flarebot.mod;
 
-public class SeverityLevel {
+import net.dv8tion.jda.core.entities.Member;
+
+import java.util.function.Consumer;
+
+public enum SeverityLevel implements Consumer<Member> {
+    NONE(m -> {}),
+    SMALL(StrikeCounter::strike),
+    MEDIUM(m -> StrikeCounter.strike(m, 2)),
+    HIGH(m -> StrikeCounter.strike(m, 3)),
+    INSTABAN(m -> m.getGuild().getController().ban(m, 14));
+
+    private Consumer<Member> operation;
+
+    SeverityLevel(Consumer<Member> operation){
+        this.operation = operation;
+    }
+
+    @Override
+    public void accept(Member member) {
+        operation.accept(member);
+    }
 }
---
+++ src/main/java/com/bwfcwalshy/flarebot/mod/StrikeCounter.java
@@ -0,0 +1,13 @@
+package com.bwfcwalshy.flarebot.mod;
+
+import net.dv8tion.jda.core.entities.Member;
+
+public class StrikeCounter {
+    public static void strike(Member member, int i) {
+
+    }
+
+    public static void strike(Member member) {
+
+    }
+}
