4aafc3e6c8259609cc665e41c8293beb27659300
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2017-03-06 20:32:15 +0000

    * Forgot it had an internal perm check *whistles*

--- src/main/java/com/bwfcwalshy/flarebot/commands/administrator/PinCommand.java
+++ src/main/java/com/bwfcwalshy/flarebot/commands/administrator/PinCommand.java
@@ -9,23 +9,17 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
-import java.util.List;
-
 public class PinCommand implements Command {
 
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
         if(args.length == 1){
-            if(FlareBot.getInstance().getPermissions(channel).hasPermission(member, "flarebot.pin")){
-                String messageId = args[0].replaceAll("[^0-9]", "");
-                Message msg = channel.getMessageById(messageId).complete();
-                msg.pin().queue();
-                channel.getHistory().retrievePast(1).complete().get(0).delete().queue();
-            }
+            String messageId = args[0].replaceAll("[^0-9]", "");
+            Message msg = channel.getMessageById(messageId).complete();
+            msg.pin().queue();
+            channel.getHistory().retrievePast(1).complete().get(0).delete().queue();
         }else{
-            if(FlareBot.getInstance().getPermissions(channel).hasPermission(member, "flarebot.pin")){
-                MessageUtils.sendErrorMessage("Usage: `pin (messageId)`", channel);
-            }
+            MessageUtils.sendErrorMessage("Usage: `pin (messageId)`", channel);
         }
     }
 
