abab6cf571849f774b638d4507187e20e47d3e1f
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-04-02 22:21:25 +0200

    Better error handling in JoinCommand

--- src/main/java/com/bwfcwalshy/flarebot/commands/general/JoinCommand.java
+++ src/main/java/com/bwfcwalshy/flarebot/commands/general/JoinCommand.java
@@ -21,22 +21,19 @@ public class JoinCommand implements Command {
                 channel.sendMessage("Currently connecting to a voice channel! Try again soon!").queue();
                 return;
             }
-            if(channel.getGuild().getSelfMember().getVoiceState().inVoiceChannel() && !channel.getGuild().getSelfMember().getVoiceState().getAudioChannel().getId()
-                    .equals(member.getVoiceState().getAudioChannel().getId()) && !FlareBot.getInstance().getPermissions(channel).hasPermission(member, "flarebot.join.other")){
+            if (channel.getGuild().getSelfMember().getVoiceState().inVoiceChannel() && !channel.getGuild().getSelfMember().getVoiceState().getAudioChannel().getId()
+                    .equals(member.getVoiceState().getAudioChannel().getId()) && !FlareBot.getInstance().getPermissions(channel).hasPermission(member, "flarebot.join.other")) {
                 channel.sendMessage(new EmbedBuilder().setColor(Color.red).setDescription("You need the permission `flarebot.join.other` for me to join your voice channel while I'm in one!")
-                    .build()).queue();
+                        .build()).queue();
                 return;
             }
             if (channel.getGuild().getSelfMember().hasPermission(member.getVoiceState().getChannel(), Permission.VOICE_CONNECT) &&
                     channel.getGuild().getSelfMember().hasPermission(member.getVoiceState().getChannel(), Permission.VOICE_SPEAK)) {
-                if (member.getVoiceState().getChannel().getMembers().size() >= member.getVoiceState().getChannel().getUserLimit() &&
-                        !member.getGuild().getSelfMember().hasPermission(member.getVoiceState().getChannel(), Permission.MANAGE_CHANNEL)) {
-                    channel.sendMessage(new EmbedBuilder().setDescription("We can't join :(\n\n" +
-                            "The channel user limit has been reached and we don't have the 'Manage Channel' permission to " +
-                            "bypass it!").setColor(Color.red).build()).queue();
-                    return;
+                try {
+                    channel.getGuild().getAudioManager().openAudioConnection(member.getVoiceState().getChannel());
+                } catch (Exception e) {
+                    channel.sendMessage("Error: `" + e.getMessage() + "`").queue();
                 }
-                channel.getGuild().getAudioManager().openAudioConnection(member.getVoiceState().getChannel());
             } else
                 channel.sendMessage("I do not have permission to " + (!channel.getGuild().getSelfMember().hasPermission(member.getVoiceState().getChannel(), Permission.VOICE_CONNECT) ?
                         "connect" : "speak") + " in your voice channel!").queue();
