0e653f02cbc9aa33d963ef580f1b48bd84b4c88c
William Oldham <wegg7250@gmail.com>
2017-05-06 20:31:01 +0100

    Deprecate search and add resume command

---
+++ src/main/java/stream/flarebot/flarebot/commands/music/ResumeCommand.java
@@ -0,0 +1,56 @@
+package stream.flarebot.flarebot.commands.music;
+
+import com.arsenarsen.lavaplayerbridge.PlayerManager;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import stream.flarebot.flarebot.FlareBot;
+import stream.flarebot.flarebot.MessageUtils;
+import stream.flarebot.flarebot.commands.Command;
+import stream.flarebot.flarebot.commands.CommandType;
+
+/**
+ * Created by william on 06/05/17.
+ */
+public class ResumeCommand implements Command {
+
+    private PlayerManager musicManager;
+
+    public ResumeCommand(FlareBot bot) {
+        this.musicManager = bot.getMusicManager();
+    }
+
+    @Override
+    public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
+        boolean playing = musicManager.getPlayer(channel.getGuild().getId()).getPlayingTrack() != null;
+        boolean paused = musicManager.getPlayer(channel.getGuild().getId()).getPaused();
+        if (!playing && !paused) {
+            MessageUtils.sendErrorMessage("There is no music playing!", channel);
+        } else {
+            musicManager.getPlayer(channel.getGuild().getId()).play();
+            channel.sendMessage("Resuming...!").queue();
+        }
+    }
+
+    @Override
+    public String getCommand() {
+        return "resume";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Resumes your playlist";
+    }
+
+    @Override
+    public String getUsage() {
+        return "`{%}resume` - Resumes the playlist";
+    }
+
+    @Override
+    public CommandType getType() {
+        return CommandType.MUSIC;
+    }
+
+}
--- src/main/java/stream/flarebot/flarebot/commands/music/SearchCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/music/SearchCommand.java
@@ -4,30 +4,15 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
-import stream.flarebot.flarebot.FlareBot;
+import stream.flarebot.flarebot.MessageUtils;
 import stream.flarebot.flarebot.commands.Command;
 import stream.flarebot.flarebot.commands.CommandType;
-import stream.flarebot.flarebot.music.VideoThread;
 
 public class SearchCommand implements Command {
 
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
-        if (args.length == 0) {
-            channel.sendMessage(sender.getAsMention() + " Usage: " +
-                    FlareBot.getPrefixes().get(channel.getGuild().getId()) + "search <term>").queue();
-        } else if (args.length >= 1) {
-            if (args[0].startsWith("http") || args[0].startsWith("www.")) {
-                VideoThread.getThread(args[0], channel, sender).start();
-            } else {
-                String term = "";
-                for (String s : args) {
-                    term += s + " ";
-                }
-                term = term.trim();
-                VideoThread.getSearchThread(term, channel, sender).start();
-            }
-        }
+        MessageUtils.sendErrorMessage("This command is deprecated!", channel);
     }
 
     @Override
