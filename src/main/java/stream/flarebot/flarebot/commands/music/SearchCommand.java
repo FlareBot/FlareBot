024aae45e46fc27634b73470ee52ef48181d0b37
William Oldham <wegg7250@gmail.com>
2017-05-06 21:41:21 +0100

    Queue it

--- src/main/java/stream/flarebot/flarebot/commands/music/SearchCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/music/SearchCommand.java
@@ -24,7 +24,7 @@ public class SearchCommand implements Command {
 
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
-        channel.sendMessage(new EmbedBuilder().setColor(Color.YELLOW).setDescription("This is deprecated! Please use _play instead!").build());
+        channel.sendMessage(new EmbedBuilder().setColor(Color.YELLOW).setDescription("This is deprecated! Please use _play instead!").build()).queue();
         if (args.length > 0) {
             if (args[0].startsWith("http") || args[0].startsWith("www.")) {
                 VideoThread.getThread(args[0], channel, sender).start();
