1532ddb60d1cbf09de2f652b46c9e2a7e3d2ae69
William Oldham <wegg7250@gmail.com>
2017-05-07 12:34:38 +0100

    Fix null error on userinfo

--- src/main/java/stream/flarebot/flarebot/commands/general/UserInfoCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/general/UserInfoCommand.java
@@ -17,9 +17,14 @@ public class UserInfoCommand implements Command {
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
         String id;
-        if (args.length != 1)
+        if (args.length != 1) {
             id = sender.getId();
-        else id = args[0].replaceAll("[^0-9]", "");
+        } else {
+            id = args[0].replaceAll("[^0-9]", "");
+        }
+        if (id.isEmpty()) {
+            MessageUtils.sendErrorMessage("You must mention the user!", channel);
+        }
         User user = FlareBot.getInstance().getUserByID(id);
         if (user == null) {
             MessageUtils.sendErrorMessage("We cannot find that user!", channel);
