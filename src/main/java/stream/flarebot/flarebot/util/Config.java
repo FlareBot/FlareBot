711e87dd16ea6fe7ba8a96a0fb74bdf71b2c371d
William Oldham <wegg7250@gmail.com>
2017-05-09 19:30:46 +0100

    Format

--- src/main/java/stream/flarebot/flarebot/util/Config.java
+++ src/main/java/stream/flarebot/flarebot/util/Config.java
@@ -18,13 +18,13 @@ public class Config {
     private File configFile;
     private JsonObject object;
 
-    public Config(String fileName){
+    public Config(String fileName) {
         this.configFile = new File((fileName.endsWith(".json") ? fileName : fileName + ".json"));
         try {
-            if(!configFile.exists())
+            if (!configFile.exists())
                 configFile.createNewFile();
             JsonElement element = new JsonParser().parse(new FileReader(configFile));
-            if(element != null && !element.isJsonNull() && element.getAsJsonObject() != null)
+            if (element != null && !element.isJsonNull() && element.getAsJsonObject() != null)
                 this.object = element.getAsJsonObject();
             else
                 this.object = new JsonObject();
@@ -33,61 +33,61 @@ public class Config {
         }
     }
 
-    public JsonElement getElement(String s){
-        if(this.object.has(s)){
+    public JsonElement getElement(String s) {
+        if (this.object.has(s)) {
             return this.object.get(s);
-        }else{
-            if(s.contains(".")){
+        } else {
+            if (s.contains(".")) {
                 String[] split = s.split("\\.");
                 JsonObject element = object.getAsJsonObject();
-                for(String subPath : split){
-                    if(element.has(subPath)){
-                        if(element.get(subPath).isJsonObject() && !(split[split.length-1].equals(subPath))) {
+                for (String subPath : split) {
+                    if (element.has(subPath)) {
+                        if (element.get(subPath).isJsonObject() && !(split[split.length - 1].equals(subPath))) {
                             element = element.get(subPath).getAsJsonObject();
                             continue;
                         }
                         return element.get(subPath);
-                    }else{
+                    } else {
                         throw new IllegalArgumentException("The specified member does not exist! Passed path '" + s + "', none-existent path: '" + subPath + "'");
                     }
                 }
-            }else{
+            } else {
                 throw new IllegalArgumentException("The specified member does not exist! Passed path '" + s + "'");
             }
         }
         return null;
     }
 
-    public JsonElement getElement(String path, JsonElement jsonElement){
-        if(this.object.has(path)){
+    public JsonElement getElement(String path, JsonElement jsonElement) {
+        if (this.object.has(path)) {
             return this.object.get(path);
-        }else{
-            if(path.contains(".")){
+        } else {
+            if (path.contains(".")) {
                 String[] split = path.split("\\.");
-                String lastSubPath = path.substring(path.lastIndexOf('.')+1);
+                String lastSubPath = path.substring(path.lastIndexOf('.') + 1);
                 JsonObject element = object;
-                for(int i = 0; i < split.length; i++){
+                for (int i = 0; i < split.length; i++) {
                     String subPath = split[i];
-                    if(element.has(subPath)){
-                        if(element.get(subPath).isJsonObject() && !(lastSubPath.equals(subPath))) {
+                    if (element.has(subPath)) {
+                        if (element.get(subPath).isJsonObject() && !(lastSubPath.equals(subPath))) {
                             element = element.get(subPath).getAsJsonObject();
                             continue;
                         }
                         return element.get(subPath);
-                    }else{
-                        if(subPath.equals(lastSubPath)) {
+                    } else {
+                        if (subPath.equals(lastSubPath)) {
                             element.add(subPath, jsonElement);
                             return element.get(subPath);
                         } else {
                             element.add(subPath, jsonElement);
-                            if(element.get(subPath).isJsonObject())
+                            if (element.get(subPath).isJsonObject())
                                 element = element.get(subPath).getAsJsonObject();
                         }
 
                     }
                 }
                 return element;
-            }else{
+            } else {
                 object.add(path, jsonElement);
                 return object.get(path);
                 //throw new IllegalArgumentException("The specified member does not exist! Passed path '" + path + "'");
@@ -95,70 +95,72 @@ public class Config {
         }
     }
 
-    public String getString(String s){
+    public String getString(String s) {
         return this.getElement(s).getAsString();
     }
 
-    public int getInt(String s){
+    public int getInt(String s) {
         return this.getElement(s).getAsInt();
     }
 
-    public List<String> getStringList(String s){
+    public List<String> getStringList(String s) {
         List<String> list = new ArrayList<>();
         this.getElement(s).getAsJsonArray().forEach(element -> list.add(element.getAsString()));
         return list;
     }
 
-    public JsonArray getArray(String s){
+    public JsonArray getArray(String s) {
         return this.getElement(s).getAsJsonArray();
     }
 
-    public JsonObject getObject(String s){
+    public JsonObject getObject(String s) {
         return this.getElement(s).getAsJsonObject();
     }
 
     public boolean exists(String s) {
         try {
             return getElement(s) != null;
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return false;
         }
     }
 
     /**
      * Set the JsonObject at the specified path to the Object passed (Serialized if needed).
+     *
      * @param path Path to the JsonObject
-     * @param obj Object to set
+     * @param obj  Object to set
      */
     public void set(String path, Object obj) {
         String lastSubPath = path;
-        if(path.contains("."))
+        if (path.contains("."))
             lastSubPath = path.substring(0, path.lastIndexOf('.'));
         JsonElement element = getElement(lastSubPath, new JsonObject());
 
-        if(element.isJsonObject())
+        if (element.isJsonObject())
             element.getAsJsonObject().add(lastSubPath, parser.parse(gson.toJson(obj)));
         else
             throw new IllegalArgumentException("Make sure the element at the specified path is a JsonObject not a JsonArray!!");
-            //element.getAsJsonArray().add(parser.parse(gson.toJson(obj)));
+        //element.getAsJsonArray().add(parser.parse(gson.toJson(obj)));
     }
 
     /**
      * Add the Object passed (Serialized if needed) to the JsonObject at the specified path.
+     *
      * @param path Path to the JsonArray
-     * @param obj Object to set
+     * @param obj  Object to set
      */
-    public void add(String path, Object obj){
+    public void add(String path, Object obj) {
         JsonElement element = getElement(path, new JsonArray());
 
-        if(element.isJsonObject())
+        if (element.isJsonObject())
             throw new IllegalArgumentException("Make sure the element at the specified path is a JsonArray not a JsonObject!!");
         else
             element.getAsJsonArray().add(parser.parse(gson.toJson(obj)));
     }
 
-    public void save(){
-        try (FileWriter writer = new FileWriter(configFile)){
+    public void save() {
+        try (FileWriter writer = new FileWriter(configFile)) {
             writer.write(object.toString());
             writer.close();
         } catch (IOException e) {
