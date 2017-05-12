9d32a5556554c059506739934b6677a58c57264a
William Oldham <wegg7250@gmail.com>
2017-05-12 17:05:55 +0100

    Add ability to pin msgs in an embed

--- src/main/java/stream/flarebot/flarebot/commands/administrator/PinCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/administrator/PinCommand.java
@@ -1,5 +1,6 @@
 package stream.flarebot.flarebot.commands.administrator;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
@@ -12,9 +13,15 @@ public class PinCommand implements Command {
 
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
-        if (args.length == 1) {
+        if (args.length == 1 && !args[0].replaceAll("[^0-9]", "").isEmpty()) {
             String messageId = args[0].replaceAll("[^0-9]", "");
             Message msg = channel.getMessageById(messageId).complete();
+            msg.pin().complete();
+            channel.getHistory().retrievePast(1).complete().get(0).delete().queue();
+        } else if (args.length != 0){
+            String pinMessage = MessageUtils.getMessage(args, 0);
+            Message msg = channel.sendMessage(new EmbedBuilder().setTitle(sender.getName(), null)
+                    .setThumbnail(MessageUtils.getAvatar(sender)).setDescription(pinMessage).build()).complete();
             msg.pin().complete();
             channel.getHistory().retrievePast(1).complete().get(0).delete().queue();
         } else {
@@ -34,7 +41,7 @@ public class PinCommand implements Command {
 
     @Override
     public String getUsage() {
-        return "`{%}pin <messageID>` - Pins a message";
+        return "`{%}pin <messageID|message>` - Pins a message either by ID or by typing a message";
     }
 
     @Override
