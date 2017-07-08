f0affcb94c78c8db2fb5fd0e05afe89051c666f5
William Oldham <wegg7250@gmail.com>
2017-07-08 11:09:48 +0100

    Make poll embeds better in code and in discord

--- src/main/java/stream/flarebot/flarebot/objects/Poll.java
+++ src/main/java/stream/flarebot/flarebot/objects/Poll.java
@@ -1,6 +1,9 @@
 package stream.flarebot.flarebot.objects;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.TextChannel;
+import org.apache.commons.lang3.text.WordUtils;
+import stream.flarebot.flarebot.FlareBot;
 
 import java.awt.Color;
 import java.time.LocalDateTime;
@@ -68,8 +71,11 @@ public class Poll {
     public EmbedBuilder getPollEmbed(String title, String description) {
         EmbedBuilder builder = new EmbedBuilder().setTitle(title, null).setDescription(description).addField("Question", getQuestion(), false);
         getPollOptions().forEach(option -> builder.addField("Option " + (getPollOptions().indexOf(option) + 1), option.getOption() + "\nVotes: " + option.getVotes(), true));
-        builder.setColor(pollColor).addBlankField(false).addField("End", (isClosed() ? "Closed" : "The poll will be ending at `" + FlareBot.getInstance().formatTime(getEndTime()) + "`"),
-                false).addField("Total Votes", String.valueOf(getPollOptions().stream().mapToInt(PollOption::getVotes).sum()), true);
+        builder.setColor(pollColor)
+                .addBlankField(false)
+                .addField("End", (isClosed() ? "Closed" : "The poll will be ending at `" + FlareBot.getInstance().formatTime(getEndTime()) + "`"),false)
+                .addField("Question", WordUtils.capitalizeFully(this.question), false)
+                .addField("Total Votes", String.valueOf(getPollOptions().stream().mapToInt(PollOption::getVotes).sum()), true);
         return builder;
     }
 
@@ -78,7 +84,11 @@ public class Poll {
         orderedOptions.sort((o1, o2) -> o2.getVotes() - o1.getVotes());
         EmbedBuilder builder = new EmbedBuilder().setColor(Color.red).setTitle("Poll Closed", null).setDescription("The poll has been closed!\nHere are the results: ");
         orderedOptions.forEach(option -> builder.addField(option.getOption(), "Final votes: " + option.getVotes(), false));
-        builder.addBlankField(false).addField("Total Votes", String.valueOf(getPollOptions().stream().mapToInt(PollOption::getVotes).sum()), false).setColor(Color.red);
+        builder.addBlankField(false)
+                .addField("Question", WordUtils.capitalizeFully(this.question), false)
+                .addField("Total Votes", String.valueOf(getPollOptions().stream().mapToInt(PollOption::getVotes).sum()), false)
+                .setColor(Color.RED)
+        ;
         return builder;
     }
 
