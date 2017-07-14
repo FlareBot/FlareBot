a864db79ddd26e2a2cb8c7e3c024f49808a2a2ba
William Oldham <wegg7250@gmail.com>
2017-07-14 16:50:36 +0100

    Remove sub-perm for now

--- src/main/java/stream/flarebot/flarebot/commands/moderation/PruneCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/moderation/PruneCommand.java
@@ -20,37 +20,32 @@ public class PruneCommand implements Command {
     public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
         if (args.length != 0) {
             if (args[0].equalsIgnoreCase("server") && args.length == 2) {
-                if (getPermissions(channel).hasPermission(member, "flarebot.prune.server")) {
-                    int amount;
-                    try {
-                        amount = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException e) {
-                        MessageUtils.sendErrorMessage("Please enter a valid amount of days!", channel);
-                        return;
-                    }
-
-                    if (amount == 0) {
-                        MessageUtils.sendErrorMessage("The amount of days has to be more that 0!", channel);
-                        return;
-                    }
-
-                    int userSize = guild.getGuild().getPrunableMemberCount(amount).complete();
-                    channel.sendMessage(MessageUtils.getEmbed(sender)
-                            .setColor(Color.RED)
-                            .setDescription(GeneralUtils.formatCommandPrefix(channel, "Are you sure you want to prune " + userSize + " members?\n" +
-                                    "To confirm type `{%}prune confirm` within 1 minute!"))
-                            .build()).queue();
-
-                    ConfirmUtil.pushAction(sender.getId(),
-                            new RestActionWrapper(guild.getGuild().getController()
-                                    .prune(amount)
-                                    .reason("Pruned by user: " + MessageUtils.getTag(sender)), this.getClass()));
-                    return;
-
-                } else {
-                    MessageUtils.sendErrorMessage("You need the permission `flarebot.prune.server` to do this!", channel);
+                // Re-add sub-permission when roles is added
+                int amount;
+                try {
+                    amount = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    MessageUtils.sendErrorMessage("Please enter a valid amount of days!", channel);
                     return;
                 }
+
+                if (amount == 0) {
+                    MessageUtils.sendErrorMessage("The amount of days has to be more that 0!", channel);
+                    return;
+                }
+
+                int userSize = guild.getGuild().getPrunableMemberCount(amount).complete();
+                channel.sendMessage(MessageUtils.getEmbed(sender)
+                        .setColor(Color.RED)
+                        .setDescription(GeneralUtils.formatCommandPrefix(channel, "Are you sure you want to prune " + userSize + " members?\n" +
+                                "To confirm type `{%}prune confirm` within 1 minute!"))
+                        .build()).queue();
+
+                ConfirmUtil.pushAction(sender.getId(),
+                        new RestActionWrapper(guild.getGuild().getController()
+                                .prune(amount)
+                                .reason("Pruned by user: " + MessageUtils.getTag(sender)), this.getClass()));
+                return;
             } else if (args[0].equalsIgnoreCase("confirm")) {
                 if (ConfirmUtil.checkExists(sender.getId(), this.getClass())) {
                     ConfirmUtil.get(sender.getId(), this.getClass()).queue();
