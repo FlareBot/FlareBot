424f7ca8a4261d42ad807c7a97f3636f1581d8ba
WalshyDev <bwfcwalshy@gmail.com>
2017-08-24 17:04:44 +0100

    * Update JDA

--- src/main/java/stream/flarebot/flarebot/commands/secret/LogsCommands.java
+++ src/main/java/stream/flarebot/flarebot/commands/secret/LogsCommands.java
@@ -1,6 +1,5 @@
 package stream.flarebot.flarebot.commands.secret;
 
-import stream.flarebot.flarebot.FlareBot;
 import stream.flarebot.flarebot.commands.Command;
 import stream.flarebot.flarebot.commands.CommandType;
 import net.dv8tion.jda.core.MessageBuilder;
@@ -10,17 +9,12 @@ import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
 import java.io.File;
-import java.io.IOException;
 
 public class LogsCommands implements Command {
     @Override
     public void onCommand(User sender, TextChannel channel, Message message, String[] args, Member member) {
         if (getPermissions(channel).isCreator(sender)) {
-            try {
-                channel.sendFile(new File("latest.log"), new MessageBuilder().append('\u200B').build()).queue();
-            } catch (IOException e) {
-                FlareBot.LOGGER.error("Could not send logs", e);
-            }
+            channel.sendFile(new File("latest.log"), new MessageBuilder().append('\u200B').build()).queue();
         }
     }
 
