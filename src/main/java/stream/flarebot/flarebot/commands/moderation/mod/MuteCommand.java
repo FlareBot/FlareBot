87bbec53b6bef60a4ec2dc9458ad03257c7949f2
weeryan17 <weeryan17@users.noreply.github.com>
2017-09-04 16:14:50 -0500

    Sometimes i hate github

--- src/main/java/stream/flarebot/flarebot/commands/moderation/mod/MuteCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/moderation/mod/MuteCommand.java
@@ -12,6 +12,7 @@ import org.joda.time.format.PeriodFormatterBuilder;
 import stream.flarebot.flarebot.FlareBot;
 import stream.flarebot.flarebot.commands.Command;
 import stream.flarebot.flarebot.commands.CommandType;
+import stream.flarebot.flarebot.mod.Punishment;
 import stream.flarebot.flarebot.objects.GuildWrapper;
 import stream.flarebot.flarebot.scheduler.RestActionTask;
 import stream.flarebot.flarebot.util.GeneralUtils;
@@ -20,50 +21,58 @@ import stream.flarebot.flarebot.util.MessageUtils;
 import java.awt.Color;
 
 public class MuteCommand implements Command {
+
     @Override
     public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
-        if(args.length == 0){
+        if (args.length == 0) {
             MessageUtils.getUsage(this, channel, sender).queue();
         } else {
             User user = GeneralUtils.getUser(args[0], guild.getGuildId());
-            if(user == null){
+            if (user == null) {
                 MessageUtils.sendErrorMessage("Invalid user!!", channel);
                 return;
             }
-            if(args.length >= 2){
-                long mills;
-                EmbedBuilder eb = new EmbedBuilder();
-                PeriodFormatter formatter = new PeriodFormatterBuilder()
-                        .appendDays().appendSuffix("d")
-                        .appendHours().appendSuffix("h")
-                        .appendMinutes().appendSuffix("m")
-                        .appendSeconds().appendSuffix("s")
-                        .toFormatter();
-                Period period = formatter.parsePeriod(args[1]);
-                mills = period.toStandardSeconds().getSeconds() * 1000;
-                long seconds = mills / 1000;
-                long minutes = seconds / 60;
-                long hours = minutes / 60;
-                long days = hours / 24;
-                String time = days + " days " + hours % 24 + " hours " + minutes % 60 + " minutes " + seconds % 60 + " seconds.";
-                eb.appendDescription("Muted " + user.getAsMention() + " for " + time);
-                guild.getAutoModGuild().muteUser(guild.getGuild(), guild.getGuild().getMember(user));
-                while (!guild.getGuild().getMember(user).getRoles().contains(guild.getMutedRole())){
-                    //nothing!
-                }
-                FlareBot.LOGGER.info("START");
-                RestAction ra = guild.getGuild().getController().removeSingleRoleFromMember(
-                        guild.getGuild().getMember(user), guild.getMutedRole());
-                new RestActionTask(ra, "Unmute Member: " + user.getName()).delay(mills);
-                eb.setColor(Color.CYAN);
-                channel.sendMessage(eb.build()).queue();
-            } else {
-                guild.getAutoModGuild().muteUser(guild.getGuild(), guild.getGuild().getMember(user));
-                EmbedBuilder eb = new EmbedBuilder();
-                eb.appendDescription("Muted " + user.getAsMention());
-                eb.setColor(Color.CYAN);
-                channel.sendMessage(eb.build()).queue();
+            // NOT FOR v4
+//            if(args.length >= 2){
+//                long mills;
+//                EmbedBuilder eb = new EmbedBuilder();
+//                PeriodFormatter formatter = new PeriodFormatterBuilder()
+//                        .appendDays().appendSuffix("d")
+//                        .appendHours().appendSuffix("h")
+//                        .appendMinutes().appendSuffix("m")
+//                        .appendSeconds().appendSuffix("s")
+//                        .toFormatter();
+//                Period period = formatter.parsePeriod(args[1]);
+//                mills = period.toStandardSeconds().getSeconds() * 1000;
+//                long seconds = mills / 1000;
+//                long minutes = seconds / 60;
+//                long hours = minutes / 60;
+//                long days = hours / 24;
+//                String time = days + " days " + hours % 24 + " hours " + minutes % 60 + " minutes " + seconds % 60 + " seconds.";
+//                eb.appendDescription("Muted " + user.getAsMention() + " for " + time);
+//                guild.getAutoModConfig().postToModLog(user, sender, new Punishment(Punishment.EPunishment.BAN), "");
+//                guild.getAutoModGuild().muteUser(guild.getGuild(), guild.getGuild().getMember(user));
+//                while (!guild.getGuild().getMember(user).getRoles().contains(guild.getMutedRole())){
+            //nothing!
+//                }
+//                RestAction ra = guild.getGuild().getController().removeSingleRoleFromMember(
+//                        guild.getGuild().getMember(user), guild.getMutedRole());
+//                new RestActionTask(ra, "Unmute Member: " + user.getName()).delay(mills);
+//                eb.setColor(Color.CYAN);
+//                channel.sendMessage(eb.build()).queue();
+//            } else {
+
+            if (guild.getMutedRole() == null) {
+                MessageUtils.sendErrorMessage("Error getting the \"Muted\" role! Check FlareBot has permissions to create it!", channel);
+                return;
             }
+            guild.getAutoModGuild().muteUser(guild.getGuild(), guild.getGuild().getMember(user));
+            guild.getAutoModConfig().postToModLog(user, sender, new Punishment(Punishment.EPunishment.BAN), "");
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.appendDescription("Muted " + user.getAsMention());
+            eb.setColor(Color.CYAN);
+            channel.sendMessage(eb.build()).queue();
+//            }
         }
     }
 
@@ -79,7 +88,7 @@ public class MuteCommand implements Command {
 
     @Override
     public String getUsage() {
-        return "`{%}mute <user> [time]` - mutes a user for an optional amount of time";
+        return "`{%}mute <user>` - Mutes a user";
     }
 
     @Override
