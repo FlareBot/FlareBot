720c6da933e7f7043b504e082dc907e16ba8ee6c
Daniel Walsh <bwfcwalshy@gmail.com>
2017-10-17 18:37:00 +0100

    Update purge usage

--- src/main/java/stream/flarebot/flarebot/commands/moderation/PurgeCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/moderation/PurgeCommand.java
@@ -26,85 +26,6 @@ public class PurgeCommand implements Command {
     private Map<String, Long> cooldowns = new HashMap<>();
     private static final long cooldown = 60000;
 
-    // Commented out until I know the new one compiles and works.
-    /*@Override
-    public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
-        if (args.length == 1 && args[0].matches("\\d+")) {
-            if (!FlareBotManager.getInstance().getGuild(channel.getId()).getPermissions().isCreator(sender)) {
-                long calmitdood = cooldowns.computeIfAbsent(channel.getGuild().getId(), n -> 0L);
-                if (System.currentTimeMillis() - calmitdood < cooldown) {
-                    channel.sendMessage(MessageUtils.getEmbed(sender)
-                            .setDescription(String
-                                    .format("You are on a cooldown! %s seconds left!",
-                                            (cooldown - (System
-                                                    .currentTimeMillis() - calmitdood)) / 1000))
-                            .build()).queue();
-                    return;
-                }
-            }
-            int count;
-            try {
-                count = Integer.parseInt(args[0]) + 1;
-            } catch (NumberFormatException e) {
-                MessageUtils.sendErrorMessage("The number entered is too high!", channel);
-                return;
-            }
-            if (count < 2) {
-                channel.sendMessage(MessageUtils.getEmbed(sender)
-                        .setDescription("Can't purge less than 2 messages!").build()).queue();
-            }
-            List<Permission> perms = channel.getGuild().getSelfMember().getPermissions(channel);
-            if (perms.contains(Permission.MESSAGE_HISTORY) && perms.contains(Permission.MESSAGE_MANAGE)) {
-                try {
-                    MessageHistory history = new MessageHistory(channel);
-                    int toRetrieve = count;
-                    while (history.getRetrievedHistory().size() < count) {
-                        if (history.retrievePast(Math.min(toRetrieve, 100)).complete().isEmpty())
-                            break;
-                        toRetrieve -= Math.min(toRetrieve, 100);
-                        if (toRetrieve < 2)
-                            toRetrieve = 2;
-                    }
-                    int i = 0;
-                    List<Message> toDelete = new ArrayList<>();
-                    for (Message m : history.getRetrievedHistory()) {
-                        if (m.getCreationTime().plusWeeks(2).isAfter(OffsetDateTime.now())) {
-                            i++;
-                            toDelete.add(m);
-                        }
-                        if (toDelete.size() == 100) {
-                            channel.deleteMessages(toDelete).complete();
-                            toDelete.clear();
-                        }
-                    }
-                    if (!toDelete.isEmpty()) {
-                        if (toDelete.size() != 1)
-                            channel.deleteMessages(toDelete).complete();
-                        else toDelete.forEach(mssage -> mssage.delete().complete());
-                    }
-                    channel.sendMessage(MessageUtils.getEmbed(sender)
-                            .setDescription(String.format("Deleted `%s` messages!", i-1)).build())
-                            .queue(s -> new FlareBotTask("Delete Message " + s) {
-                                @Override
-                                public void run() {
-                                    s.delete().queue();
-                                }
-                            }.delay(TimeUnit.SECONDS.toMillis(5)));
-                } catch (Exception e) {
-                    channel.sendMessage(MessageUtils.getEmbed(sender)
-                            .setDescription(String
-                                    .format("Failed to bulk delete or load messages! Error: `%s`", e))
-                            .build()).queue();
-                }
-            } else {
-                channel.sendMessage("Insufficient permissions! I need `Manage Messages` and `Read Message History`")
-                        .queue();
-            }
-        } else {
-            MessageUtils.sendUsage(this, channel, sender);
-        }
-    }*/
-
     @Override
     public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
         if(args.length >= 2) {
@@ -186,7 +107,7 @@ public class PurgeCommand implements Command {
 
     @Override
     public String getUsage() {
-        return "`{%}purge <messages>` - Purges a certain amount of messages";
+        return "`{%}purge <'all'/user> <amount>` - Purges a certain amount of messages.";
     }
 
     @Override
