e17abd10132f4881b0a7aada31c5398d000a33e9
Daniel Walsh <bwfcwalshy@gmail.com>
2017-10-17 10:32:30 +0100

    Update to have the ability to purge a user.

--- src/main/java/stream/flarebot/flarebot/commands/moderation/PurgeCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/moderation/PurgeCommand.java
@@ -26,7 +26,8 @@ public class PurgeCommand implements Command {
     private Map<String, Long> cooldowns = new HashMap<>();
     private static final long cooldown = 60000;
 
-    @Override
+    // Commented out until I know the new one compiles and works.
+    /*@Override
     public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
         if (args.length == 1 && args[0].matches("\\d+")) {
             if (!FlareBotManager.getInstance().getGuild(channel.getId()).getPermissions().isCreator(sender)) {
@@ -82,7 +83,7 @@ public class PurgeCommand implements Command {
                         else toDelete.forEach(mssage -> mssage.delete().complete());
                     }
                     channel.sendMessage(MessageUtils.getEmbed(sender)
-                            .setDescription(String.format("Deleted `%s` messages!", i)).build())
+                            .setDescription(String.format("Deleted `%s` messages!", i-1)).build())
                             .queue(s -> new FlareBotTask("Delete Message " + s) {
                                 @Override
                                 public void run() {
@@ -101,6 +102,80 @@ public class PurgeCommand implements Command {
             }
         } else {
             MessageUtils.sendUsage(this, channel, sender);
+        }
+    }*/
+
+    @Override
+    public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
+        if(args.length >= 2) {
+            // clean all 5
+            User targetUser = null;
+            if(!args[0].equalsIgnoreCase("all")) {
+                targetuser = GeneralUtils.getUser(args[0], guild.getGuildId(), true);
+                if(targetuser == null) {
+                    MessageUtils.sendErrormessage("That target user cannot be found, try mentioning them, using the user ID or using `all` to clear the entire chat.", channel);
+                    return;
+                }                
+            }
+
+            int amount = GeneralUtils.getInt(args[1], -1);
+
+            // 2 messages min
+            if(amount < 2) {
+                MessageUtils.sendErrormessage("You must purge at least 2 messages, please give a vaid purge amount.", channel);
+                return;
+            }
+
+            // This will be a successful delete so limit here.
+            if (!FlareBotManager.getInstance().getGuild(channel.getId()).getPermissions().isCreator(sender)) {
+                long riotPolice = cooldowns.computeIfAbsent(channel.getGuild().getId(), n -> 0L);
+                if (System.currentTimeMillis() - riotPolice < cooldown) {
+                    channel.sendMessage(MessageUtils.getEmbed(sender)
+                            .setDescription(String
+                                    .format("You are on a cooldown! %s seconds left!",
+                                            (cooldown - (System
+                                                    .currentTimeMillis() - riotPolice)) / 1000))
+                            .build()).queue();
+                    return;
+                }
+            }
+
+            if(!guild.getGuild().getSelfMember().hasPermission(Permission.MANAGE_MESSAGES, Permission.MESSAGE_HISTORY)) {
+                MessageUtils.sendErrorMessage("I do not have the perms `Manage Messages` and `Read Message History` please make sure I have these and do the command again.", channel);
+                return;
+            }
+            MessageHistory history = new MessageHistory(channel);
+            int toRetrieve = amount + 1;
+            int i = 0;
+            outer:
+            while(toRetrieve > 0) { // I don't really know if this should be min... 
+                                    // since deleting 10 of someone could be like 100 back yet this would request it 10 times. 
+                                    // For now I will just request 100 here each time.
+                if(history.retrievePast(100).complete().isEmpty()) {
+                    break;
+                }
+
+                List<Message> toDelete = new ArrayList<>();
+                for(Message msg : history.getRetrievedHistory()) {
+                    if(msg.getCreationTime().plusWeeks(2).isBefore(OffsetDateTime.now())) break outer;
+                    if(target != null && msg.getAuthor().getId().equals(target.getId()))
+                        toDelete.add(msg);
+                    else
+                        toDelete.add(msg);
+                    i++;
+                }
+                channel.deleteMessages(toDelete).complete();
+                toRetrieve -= toDelete.size();
+                toDelete.clear();
+            }
+            channel.sendMessage(MessageUtils.getEmbed(sender)
+                            .setDescription(String.format("Deleted `%s` messages!", i-1)).build())
+                            .queue(s -> new FlareBotTask("Delete Message " + s) {
+                                @Override
+                                public void run() {
+                                    s.delete().queue();
+                                }
+                            }.delay(TimeUnit.SECONDS.toMillis(5)));
         }
     }
 
