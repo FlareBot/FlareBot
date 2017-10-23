6584e09ad61f94f1c9e78540572ea76623277595
WalshyDev <bwfcwalshy@gmail.com>
2017-10-23 19:40:00 +0100

    Fix the announcement command (also changed name) * The announcement command accepted no args (Why Arsen... why) * Made more sense as postupdate, alias is announcement.

--- src/main/java/stream/flarebot/flarebot/FlareBot.java
+++ src/main/java/stream/flarebot/flarebot/FlareBot.java
@@ -73,6 +73,7 @@ import stream.flarebot.flarebot.commands.secret.DisableCommandCommand;
 import stream.flarebot.flarebot.commands.secret.EvalCommand;
 import stream.flarebot.flarebot.commands.secret.GuildCommand;
 import stream.flarebot.flarebot.commands.secret.LogsCommand;
+import stream.flarebot.flarebot.commands.secret.PostUpdateCommand;
 import stream.flarebot.flarebot.commands.secret.QueryCommand;
 import stream.flarebot.flarebot.commands.secret.QuitCommand;
 import stream.flarebot.flarebot.commands.secret.ShardRestartCommand;
@@ -519,7 +520,7 @@ public class FlareBot {
         registerCommand(new DisableCommandCommand());
 
         registerCommand(new TagsCommand());
-        registerCommand(new AnnounceCommand());
+        registerCommand(new PostUpdateCommand());
 
         ApiFactory.bind();
 
