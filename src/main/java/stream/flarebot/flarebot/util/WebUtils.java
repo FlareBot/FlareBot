2a356c91324ff208ae303a2dc436c272d30959d8
WalshyDev <bwfcwalshy@gmail.com>
2017-11-12 20:25:49 +0000

    Fix response leaks

--- src/main/java/stream/flarebot/flarebot/util/WebUtils.java
+++ src/main/java/stream/flarebot/flarebot/util/WebUtils.java
@@ -9,6 +9,7 @@ import okhttp3.Response;
 import org.json.JSONObject;
 import stream.flarebot.flarebot.FlareBot;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
@@ -22,12 +23,15 @@ public class WebUtils {
 
     private static final Callback defaultCallback = new Callback() {
         @Override
+        @ParametersAreNonnullByDefault
         public void onFailure(Call call, IOException e) {
             FlareBot.LOGGER.error("Error for " + call.request().method() + " request to " + call.request().url(), e);
         }
 
         @Override
+        @ParametersAreNonnullByDefault
         public void onResponse(Call call, Response response) throws IOException {
+            response.close();
             FlareBot.LOGGER.debug("Reponse for " + call.request().method() + " request to " + call.request().url());
         }
     };
