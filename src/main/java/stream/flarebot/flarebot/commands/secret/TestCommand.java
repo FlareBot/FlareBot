c982e1cf6ed893ca25959760f5ef51df19fa3501
Unknown <theweeryan17@gmail.com>
2017-12-04 12:47:37 -0600

    ok?

---
+++ src/main/java/stream/flarebot/flarebot/commands/secret/TestCommand.java
@@ -0,0 +1,37 @@
+package stream.flarebot.flarebot.commands.secret;
+
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import stream.flarebot.flarebot.commands.Command;
+import stream.flarebot.flarebot.commands.CommandType;
+import stream.flarebot.flarebot.objects.GuildWrapper;
+
+public class TestCommand implements Command {
+
+    @Override
+    public void onCommand(User sender, GuildWrapper guild, TextChannel channel, Message message, String[] args, Member member) {
+        // Served it's purpose once again :) Now it's free to be reused... no memes pls
+    }
+
+    @Override
+    public String getCommand() {
+        return "test";
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getUsage() {
+        return "{%}test";
+    }
+
+    @Override
+    public CommandType getType() {
+        return CommandType.SECRET;
+    }
+}
