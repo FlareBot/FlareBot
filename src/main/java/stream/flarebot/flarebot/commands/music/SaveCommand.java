aede356bd8dad9168038f1f0e0858b561ccdd09e
William Oldham <wegg7250@gmail.com>
2018-02-01 07:42:35 +0000

    Update SaveCommand.java

--- src/main/java/stream/flarebot/flarebot/commands/music/SaveCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/music/SaveCommand.java
@@ -58,7 +58,7 @@ public class SaveCommand implements Command {
         FlareBot.getInstance().getManager().savePlaylist(this,
                 channel,
                 sender.getId(),
-                this.getPermissions(channel).hasPermission(member, "flarebot.playlist.save.overwrite"),
+                this.getPermissions(channel).hasPermission(member, "flarebot.queue.save.overwrite"),
                 name,
                 tracks);
     }
