68c6703a39c42b4ab25199cf1ec3c6b769f009a6
Unknown <theweeryan17@gmail.com>
2018-02-22 10:45:21 -0600

    Cleanup

--- src/main/java/stream/flarebot/flarebot/FlareBot.java
+++ src/main/java/stream/flarebot/flarebot/FlareBot.java
@@ -33,8 +33,6 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
