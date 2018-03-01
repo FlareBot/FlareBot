c11b656c38c4c5c3dc39a0883d211f7447f62236
Daniel Walsh <bwfcwalshy@gmail.com>
2018-03-01 21:31:47 +0000

    Imports

--- src/main/java/stream/flarebot/flarebot/util/WebUtils.java
+++ src/main/java/stream/flarebot/flarebot/util/WebUtils.java
@@ -2,13 +2,17 @@ package stream.flarebot.flarebot.util;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.ConnectionPool;
 import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.json.JSONObject;
 import stream.flarebot.flarebot.FlareBot;
+import stream.flarebot.flarebot.api.GzipRequestInterceptor;
+import stream.flarebot.flarebot.web.DataInterceptor;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
