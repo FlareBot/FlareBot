e1afde29c1761053439755da97004987f6afbad1
William Oldham <wegg7250@gmail.com>
2018-03-02 11:29:12 +0000

    Import

--- src/main/java/stream/flarebot/flarebot/commands/secret/DebugCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/secret/DebugCommand.java
@@ -1,13 +1,11 @@
 package stream.flarebot.flarebot.commands.secret;
 
-import java.util.stream.Collectors;
-
-import net.dv8tion.jda.core.JDAInfo;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.impl.JDAImpl;
 import org.apache.commons.lang3.text.WordUtils;
 import stream.flarebot.flarebot.Events;
 import stream.flarebot.flarebot.FlareBot;
@@ -19,6 +17,8 @@ import stream.flarebot.flarebot.objects.GuildWrapper;
 import stream.flarebot.flarebot.util.MessageUtils;
 import stream.flarebot.flarebot.web.DataInterceptor;
 
+import java.util.stream.Collectors;
+
 public class DebugCommand implements Command {
 
     @Override
@@ -28,22 +28,22 @@ public class DebugCommand implements Command {
             return;
         }
         FlareBot fb = FlareBot.getInstance();
-    
+
         EmbedBuilder eb = MessageUtils.getEmbed();
         if (args[0].equalsIgnoreCase("flarebot") || args[0].equalsIgnoreCase("bot")) {
             eb.setTitle("Bot Debug").setDescription(String.format("Debug for FlareBot v" + fb.getVersion()
-                    + "\nUptime: %s"
-                    + "\nMemory Usage: %s"
-                    + "\nMemory Free: %s"
-                    + "\nVideo Threads: %d"
-                    + "\nCommand Threads: %d"
-                    + "Total Threads: %d"
-                    + "\n\nGuilds: %d"
-                    + "\nLoaded Guilds: %d"
-                    + "\nVoice Channels: %d"
-                    + "\nActive Voice Channels: %d"
-                    + "\nCommands Executed: %d"
-                    + "\nQueued RestActions: %s",
+                            + "\nUptime: %s"
+                            + "\nMemory Usage: %s"
+                            + "\nMemory Free: %s"
+                            + "\nVideo Threads: %d"
+                            + "\nCommand Threads: %d"
+                            + "Total Threads: %d"
+                            + "\n\nGuilds: %d"
+                            + "\nLoaded Guilds: %d"
+                            + "\nVoice Channels: %d"
+                            + "\nActive Voice Channels: %d"
+                            + "\nCommands Executed: %d"
+                            + "\nQueued RestActions: %s",
 
                     fb.getUptime(),
                     getMB(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()),
@@ -51,21 +51,21 @@ public class DebugCommand implements Command {
                     VideoThread.VIDEO_THREADS.activeCount(),
                     Events.COMMAND_THREADS.activeCount(),
                     Thread.getAllStackTraces().size(),
-                    fb.getGuildsCache().size(), 
-                    FlareBotManager.getInstance().getGuilds().size(), 
-                    fb.getConnectedVoiceChannels(), 
+                    fb.getGuildsCache().size(),
+                    FlareBotManager.getInstance().getGuilds().size(),
+                    fb.getConnectedVoiceChannels(),
                     fb.getActiveVoiceChannels(),
                     fb.getEvents().getCommandCount(),
                     getQueuedRestActions()
             ));
-            
+
             StringBuilder sb = new StringBuilder();
             for (DataInterceptor interceptor : DataInterceptor.getInterceptors())
                 sb.append(WordUtils.capitalize(interceptor.getSender().getName())).append(" - ").append(interceptor.getRequests())
                         .append(" requests").append("\n");
             eb.addField("HTTP Requests", sb.toString(), false);
         }
-        
+
         channel.sendMessage(eb.build()).queue();
     }
 
@@ -88,11 +88,11 @@ public class DebugCommand implements Command {
     public CommandType getType() {
         return CommandType.SECRET;
     }
-    
+
     private String getMB(long bytes) {
         return (bytes / 1024 / 1024) + "MB";
     }
-    
+
     private String getQueuedRestActions() {
         String queued = FlareBot.getInstance().getShards().stream()
                 .filter(shard -> !((JDAImpl) shard).getRequester().getRateLimiter().getQueuedRouteBuckets().isEmpty())
@@ -100,7 +100,7 @@ public class DebugCommand implements Command {
                 .collect(Collectors.joining(", "));
         if (queued.isEmpty())
             queued = "No queued rest actions";
-        
+
         return queued;
     }
 }
