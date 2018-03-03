7101191e9a5630374e64c044e30b2d09e8bf7ac3
Daniel Walsh <bwfcwalshy@gmail.com>
2018-03-03 13:55:06 +0000

    Update RandomUtils.java

--- src/main/java/stream/flarebot/flarebot/util/RandomUtils.java
+++ src/main/java/stream/flarebot/flarebot/util/RandomUtils.java
@@ -1,5 +1,12 @@
 package stream.flarebot.flarebot.util;
 
+import java.util.Random;
+
 public class RandomUtils {
 
+    private final Random random = new Random();
+    
+    public static String getRandomString(Collection<String> collection) {
+        return collection.toArray(new String[collection.size()]{})[random.nextInt(collection.size())];
+    }
 }
