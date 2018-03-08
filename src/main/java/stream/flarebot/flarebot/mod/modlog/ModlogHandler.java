fdf0492786c40783a683195bd931b937c31dd71c
Daniel Walsh <bwfcwalshy@gmail.com>
2018-03-08 13:32:41 +0000

    Ban should have a user check

--- src/main/java/stream/flarebot/flarebot/mod/modlog/ModlogHandler.java
+++ src/main/java/stream/flarebot/flarebot/mod/modlog/ModlogHandler.java
@@ -143,7 +143,7 @@ public class ModlogHandler {
             member = wrapper.getGuild().getMember(target);
         }
         if (channel == null) return;
-        if (member == null && modAction != ModAction.BAN && modAction != ModAction.FORCE_BAN && modAction != ModAction.UNBAN) {
+        if (member == null && modAction != ModAction.FORCE_BAN && modAction != ModAction.UNBAN) {
             MessageUtils.sendErrorMessage("That user isn't in this server!"
                     + (modAction == ModAction.KICK ? " You can forceban with `{%}forceban <id>` to keep them from coming back." : ""), channel);
             return;
