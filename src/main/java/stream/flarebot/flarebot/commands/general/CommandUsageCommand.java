5f67cd067d2faf65077591d97b2c73584fc78aae
Daniel Walsh <bwfcwalshy@gmail.com>
2018-03-09 12:22:02 +0000

    Import guild

--- src/main/java/stream/flarebot/flarebot/commands/general/CommandUsageCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/general/CommandUsageCommand.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.Guild;
 import stream.flarebot.flarebot.FlareBot;
 import stream.flarebot.flarebot.commands.Command;
 import stream.flarebot.flarebot.commands.CommandType;
