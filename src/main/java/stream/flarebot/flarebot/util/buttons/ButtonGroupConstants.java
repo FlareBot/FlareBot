04bad61e6d664267c3d5ee660f045cf0b5894cb1
Unknown <theweeryan17@gmail.com>
2018-03-24 14:05:05 -0500

    finals

--- src/main/java/stream/flarebot/flarebot/util/buttons/ButtonGroupConstants.java
+++ src/main/java/stream/flarebot/flarebot/util/buttons/ButtonGroupConstants.java
@@ -1,33 +1,34 @@
 package stream.flarebot.flarebot.util.buttons;
 
 public class ButtonGroupConstants {
-    public static String SHARDINFO = "shardinfo pages";
 
-    public static String PERMISSIONS_GROUPS = "permissions groups";
-    public static String PERMISSIONS_GROUP = "permissions group list";
-    public static String PERMISSIONS_USER_GROUPS = "permissions user groups";
-    public static String PERMISSIONS_USER_PERMISSIONS = "permissions user permissions";
-    public static String PERMISSIONS_USER_CHECK = "permissions user check";
-    public static String PERMISSIONS_LIST = "permissions list";
+    public static final String SHARDINFO = "shardinfo pages";
 
-    public static String NINO = "nino messages";
+    public static final String PERMISSIONS_GROUPS = "permissions groups";
+    public static final String PERMISSIONS_GROUP = "permissions group list";
+    public static final String PERMISSIONS_USER_GROUPS = "permissions user groups";
+    public static final String PERMISSIONS_USER_PERMISSIONS = "permissions user permissions";
+    public static final String PERMISSIONS_USER_CHECK = "permissions user check";
+    public static final String PERMISSIONS_LIST = "permissions list";
 
-    public static String VOTE_SKIP = "skip vote";
+    public static final String NINO = "nino messages";
 
-    public static String SONG = "song";
+    public static final String VOTE_SKIP = "skip vote";
 
-    public static String MODLOG_FEATURES = "modlog features";
-    public static String MODLOG_LIST = "modlog list";
+    public static final String SONG = "song";
 
-    public static String REPORTS_LIST = "reports list";
+    public static final String MODLOG_FEATURES = "modlog features";
+    public static final String MODLOG_LIST = "modlog list";
 
-    public static String REMIND_LIST = "remind list";
+    public static final String REPORTS_LIST = "reports list";
 
-    public static String HELP = "help menu";
+    public static final String REMIND_LIST = "remind list";
 
-    public static String WELCOME_LIST = "welcome list";
+    public static final String HELP = "help menu";
 
-    public static String ROLES = "roles list";
+    public static final String WELCOME_LIST = "welcome list";
 
-    public static String QUEUE = "queue list";
+    public static final String ROLES = "roles list";
+
+    public static final String QUEUE = "queue list";
 }
