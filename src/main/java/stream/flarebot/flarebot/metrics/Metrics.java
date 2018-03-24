bd06c18422afb7a489b0555ac764af739b49452d
William Oldham <wegg7250@gmail.com>
2018-03-24 19:16:53 +0000

    Make this a little more descriptive

--- src/main/java/stream/flarebot/flarebot/metrics/Metrics.java
+++ src/main/java/stream/flarebot/flarebot/metrics/Metrics.java
@@ -80,7 +80,7 @@ public class Metrics {
     public static final Counter buttonsPressed = Counter.build()
             .name("flarebot_buttons_pressed_total")
             .help("Total number of times a button was pressed")
-            .labelNames("button", "group")
+            .labelNames("button", "button_group")
             .register();
 
     // Commands
