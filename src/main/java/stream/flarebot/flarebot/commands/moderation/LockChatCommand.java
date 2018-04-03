b50fef5c548a1091ec26dfe4dd128ca62f221b79
WalshyDev <bwfcwalshy@gmail.com>
2018-04-04 00:38:05 +0100

    Make lockchat a lot nicer

--- src/main/java/stream/flarebot/flarebot/commands/moderation/LockChatCommand.java
+++ src/main/java/stream/flarebot/flarebot/commands/moderation/LockChatCommand.java
@@ -2,13 +2,18 @@ package stream.flarebot.flarebot.commands.moderation;
 
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.*;
-import stream.flarebot.flarebot.commands.Command;
-import stream.flarebot.flarebot.commands.CommandType;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.PermissionOverride;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import stream.flarebot.flarebot.commands.*;
 import stream.flarebot.flarebot.objects.GuildWrapper;
 import stream.flarebot.flarebot.util.ColorUtils;
 import stream.flarebot.flarebot.util.MessageUtils;
 import stream.flarebot.flarebot.util.general.GuildUtils;
+
+import java.util.EnumSet;
 
 public class LockChatCommand implements Command {
 
@@ -32,30 +37,26 @@ public class LockChatCommand implements Command {
             return;
         }
 
-        boolean locked;
         PermissionOverride everyoneOvr = tc.getPermissionOverride(guild.getGuild().getPublicRole());
-        if (everyoneOvr == null)
-            tc.createPermissionOverride(guild.getGuild().getPublicRole()).complete();
-        if (!everyoneOvr.getDenied().contains(Permission.MESSAGE_WRITE)) {
-            everyoneOvr.getManager().deny(Permission.MESSAGE_WRITE).queue();
-            if (tc.getPermissionOverride(guild.getGuild().getSelfMember()) == null)
-                tc.createPermissionOverride(guild.getGuild().getSelfMember()).setAllow(Permission.MESSAGE_WRITE, Permission.MESSAGE_READ).queue();
-            else
-                tc.getPermissionOverride(guild.getGuild().getSelfMember()).getManager().grant(Permission.MESSAGE_WRITE, Permission.MESSAGE_READ).queue();
-            locked = true;
-        } else {
-            everyoneOvr.getManager().grant(Permission.MESSAGE_WRITE).queue();
-            if (tc.getPermissionOverride(guild.getGuild().getSelfMember()) != null)
-                tc.getPermissionOverride(guild.getGuild().getSelfMember()).delete().queue();
-            locked = false;
-        }
+        boolean locking = everyoneOvr.getAllowed().contains(Permission.MESSAGE_WRITE);
+
+        EnumSet<Permission> perm = EnumSet.of(Permission.MESSAGE_WRITE);
+        EnumSet<Permission> empty = EnumSet.noneOf(Permission.class);
+
+        tc.putPermissionOverride(guild.getGuild().getPublicRole())
+                .setPermissions(locking ? empty : perm, locking ? perm : empty)
+                .queue();
+        tc.putPermissionOverride(guild.getGuild().getSelfMember())
+                .setPermissions(locking ? perm : empty, locking ? empty : perm)
+                .queue();
+
         if (tc.getIdLong() != channel.getIdLong())
-            channel.sendMessage(new EmbedBuilder().setColor(locked ? ColorUtils.RED : ColorUtils.GREEN)
-                    .setDescription(tc.getAsMention() + " has been " + (locked ? "locked" : "unlocked") + "!")
+            channel.sendMessage(new EmbedBuilder().setColor(locking ? ColorUtils.RED : ColorUtils.GREEN)
+                    .setDescription(tc.getAsMention() + " has been " + (locking ? "locked" : "unlocked") + "!")
                     .build()).queue();
         if (guild.getGuild().getSelfMember().hasPermission(tc, Permission.MESSAGE_WRITE))
-            channel.sendMessage(new EmbedBuilder().setColor(locked ? ColorUtils.RED : ColorUtils.GREEN)
-                    .setDescription("The chat has been " + (locked ? "locked" : "unlocked") + " by a staff member!"
+            channel.sendMessage(new EmbedBuilder().setColor(locking ? ColorUtils.RED : ColorUtils.GREEN)
+                    .setDescription("The chat has been " + (locking ? "locked" : "unlocked") + " by a staff member!"
                             + (reason != null ? "\nReason: " + reason : ""))
                     .build()).queue();
     }
